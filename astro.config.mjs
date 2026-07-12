62d5f2b1fb30739419a4aacc7e6c0c50e0abb4f0
hijo-dev-server <hijoushoku88@gmail.com>
2026-07-12 08:05:57 +0000

    フォントをAstro FontAPI経由取得に変換

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig ,fontProviders } from 'astro/config';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    fonts: [{
+        provider: fontProviders.google(),
+        name:'DotGothic16',
+        cssVariable:'--font-dotgothic16',
+        weights: [400],
+        fallbacks:['Courier New', 'monospace'],
+    },
+    {
+        provider: fontProviders.google(),
+        name:'Zen Kaku Gothic New',
+        cssVariable:'--font-zen-kaku',
+        weights: [400,500,700],
+        fallbacks:['system-ui','sans-serif'],
+    },
+]
+
+});
